c7b391053dc0661ee6703e714e8843d1b16a7a86
Balaramansethu <baalaaraman@gmail.com>
2024-11-27 04:08:34 +0530

    Intial commit - User interface till milestone

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeInterface from './components/HomeInterface/HomeInterface'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <HomeInterface/>
+    </React.Fragment>
+  )
+}
+
+export default App
